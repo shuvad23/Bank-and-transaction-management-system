3b1eb7cd95da7d8734c2c4d3be68c25fc13c154e
shuvad23 <shuvad29@gmail.com>
2026-04-27 01:40:27 +0600

    update tomal file

--- backend/core/config.py
+++ backend/core/config.py
@@ -19,7 +19,7 @@ class Settings(BaseSettings):
         "http://localhost:5173",
         "http://localhost:5000",
         "http://localhost:3000",
-        "*",
+        "https://nexbank4.netlify.app/",
     ]
 
     @property
